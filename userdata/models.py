384f846fe53d26d94a06052ecb8db869219c534c
Abhishek Kumar <abhishekk580@gmail.com>
2019-09-07 12:19:24 +0530

    chnaged models

--- userdata/models.py
+++ userdata/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class UserProfile(models.Model):
+    userid = models.CharField(max_length=100)
+    message = models.TextField()
+
+    def __str__(self):
+        return self.userid
--- userdata/views.py
+++ userdata/views.py
@@ -1,3 +1,47 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
+from django.contrib.auth import logout
+from django.http import HttpResponseRedirect
+from .models import UserProfile
 
-# Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+def signUpView(request):
+    username = request.POST['Username']
+    password = request.POST['Password']
+    email = request.POST['Email']
+
+    user = User.objects.create_user(username, email, password)
+    user.save()
+
+    return render(request, 'signup.html')
+
+def loginView(request):
+    username = request.POST['Username']
+    password = request.POST['Password']
+
+    user = authenticate(request, username=username, password=password)
+
+    data = None
+    
+    if user is not None:
+        login(request, user)
+        data = UserProfile.objects.filter(userid=request.user)
+        print(data)
+    return render(request, 'login.html', {'data': data})
+
+def logoutView(request):
+    logout(request)
+    return HttpResponseRedirect('/')
+
+def userProfileView(request, link):
+    return render(request, 'profile.html')
+
+def userMessage(request):
+    userid = request.POST['profileid']
+    message = request.POST['message']
+    userdata = UserProfile.objects.create(userid=userid, message=message)
+    userdata.save()
+    return render(request, 'thankyou.html')
